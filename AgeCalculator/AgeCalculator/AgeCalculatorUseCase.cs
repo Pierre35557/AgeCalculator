8d80c4175b512ed5a89a466a4f5b49678bf88c7b
Pierre <Pierre@warpdevelopment.com>
2020-04-20 10:24:33 +0200

    Initial Commit

---
+++ AgeCalculator/AgeCalculator/AgeCalculatorUseCase.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AgeCalculator
+{
+    [TestFixture]
+    public class AgeCalculatorUseCase
+    {
+        [TestCase("1982/09/30", "2001/10/05", 19)]
+        [TestCase("1993/10/27", "2020/04/20", 26)]
+        [TestCase("1995/06/19", "2020/04/20", 24)]
+        [TestCase("2001/05/20", "2016/05/19", 14)]
+        public void CalculateAge_GivenDate_AndTargetDate_ShouldReturnAgeInYears(string givenDate, string targetDate, int expected)
+        {
+            //Act
+            int actual = Calculate(givenDate, targetDate);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private int Calculate(string givenDate, string targetDate)
+        {
+            DateTime convertedGivenDate = Convert.ToDateTime(givenDate);
+            DateTime convertedTargetDate = Convert.ToDateTime(targetDate);
+
+            int age = convertedTargetDate.Year - convertedGivenDate.Year;
+
+            if (convertedGivenDate > convertedTargetDate.AddYears(-age))
+                age--;
+
+            return age;
+        }
+    }
+}
---
+++ AgeCalculator/AgeCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AgeCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AgeCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac7da895-fbc3-4e79-b2f7-e8b660e0d82a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
